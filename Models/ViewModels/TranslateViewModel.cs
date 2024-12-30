66336288c776b0d070fc986fc3b65fb403229caf
Lev <GitHub16102010@gmail.com>
2024-12-30 19:41:40 +0300

    fix bug of validation

--- Models/ViewModels/TranslateViewModel.cs
+++ Models/ViewModels/TranslateViewModel.cs
@@ -7,13 +7,13 @@ namespace NS_ASP.Models.ViewModels;
 public class TranslateViewModel
 {
     [Required]
-    [RegularExpression(@"[1-2]{1}[1-6]{1}", ErrorMessage = "недоступимая система счисления будущего числа(должна быть в диапазоне 1-16)")]
+    [RegularExpression(@"[1]{1}[0-6]{1}|[2-9]{1}", ErrorMessage = "недоступимая система счисления будущего числа(должна быть в диапазоне 1-16)")]
     public string System { get; set; }
 
     [Required]
     public int Number { get; set; }
 
     [Required]
-    [RegularExpression(@"[1-2]{1}[1-6]{1}", ErrorMessage = "недоступимая система счисления исходного числа(должна быть в диапазоне 1-16)")]
+    [RegularExpression(@"[1]{1}[0-6]{1}|[2-9]{1}", ErrorMessage = "недоступимая система счисления исходного числа(должна быть в диапазоне 1-16)")]
     public string SystemOfNumber { get; set; }
 }
